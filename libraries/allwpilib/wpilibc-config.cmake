9b7d11a2811a5a33e5192c85334d2d3a9837e7a5
Thad House <ThadHouse@users.noreply.github.com>
2018-02-22 13:50:50 -0800

    Adds WPILibC and HAL. Also adds wpilibj, but untested

---
+++ libraries/allwpilib/wpilibc-config.cmake
@@ -0,0 +1,9 @@
+include(CMakeFindDependencyMacro)
+find_dependency(wpiutil)
+find_dependency(ntcore)
+find_dependency(cscore)
+find_dependency(wpiHal)
+find_dependency(OpenCV)
+
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/wpilibc.cmake)
